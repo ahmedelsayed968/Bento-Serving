493092952003ffaecfa9aff8fd19222c32093f9e
ahmedelsayed968 <ahmedeltop968@gmail.com>
2024-09-18 07:51:01 +0300

    [Feat]: add simple train script for a demo

---
+++ src/SimpleBentoService/train.py
@@ -0,0 +1,13 @@
+from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
+from pathlib import Path
+import mlflow.sklearn
+if __name__ == "__main__":
+    iris = load_iris()
+    X_train = iris.data[:, :4]
+    Y_train = iris.target
+
+    model_uri = Path("models", "IrisClf")
+    model = KNeighborsClassifier()
+    model.fit(X_train, Y_train)
+    mlflow.sklearn.save_model(model, model_uri.resolve())
