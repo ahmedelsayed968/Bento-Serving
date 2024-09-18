de064aca1176926ea9aaf9ace5df679b721304f7
ahmedelsayed968 <ahmedeltop968@gmail.com>
2024-09-18 08:37:27 +0300

    [Feat]: add the saved model to Bento Registry

--- src/SimpleBentoService/train.py
+++ src/SimpleBentoService/train.py
@@ -2,6 +2,7 @@ from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
 from pathlib import Path
 import mlflow.sklearn
+import bentoml
 if __name__ == "__main__":
     iris = load_iris()
     X_train = iris.data[:, :4]
@@ -11,3 +12,4 @@ if __name__ == "__main__":
     model = KNeighborsClassifier()
     model.fit(X_train, Y_train)
     mlflow.sklearn.save_model(model, model_uri.resolve())
+    bentoml.mlflow.import_model("iris", model_uri)
